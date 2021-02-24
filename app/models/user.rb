91dbc9f7023cc846b80c013de439aec2bf6df23a
ike <kumanomisan@gmail.com>
2021-02-24 06:34:23 +0000

    user signup

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  #事前にemail（自身)をすべて小文字へ置き換え(downcase!)
+  before_save { self.email.downcase! }
+  
+  
+  # validate for name
+  validates :name, presence: true, length: { maximum: 50 }
+  
+  # validate for e-mail
+  validates :email, presence: true, length: { maximum: 255 }, 
+                    #mailアドレスの形式を正規表現で記述し対象データのフォーマット確認
+                    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    #重複禁止(caase_sensitveで大小文字を区別するかどうかを設定)
+                    uniqueness: { case_sensitive: false }
+                    
+  
+    #password_digest(暗号文)カラムを用意し、モデルにhas~を記述する
+  #利用する際には bcrypt のgemを有効かする必要がある
+  # authenticate認証メソッド
+  has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   # 'コントローラ名#アクション名'
   # ということはtopppagesコントローラと対応するindexアクション(toppages/index.html.erb)の作成が必要
   root to: "toppages#index"
+  
+  # 新規登録URLを/signupにするため設定
+  # resources :users で生成される URL である /users/new は少し格好悪く思えます。
+  # そんなときには get 'signup' のように個別に設定することで解決します。とのこと
+  get "signup", to: "users#new"
+  
+  #必要なアクションだけを設定している
+  resources :users, only: [:index, :show, :create]
 end
